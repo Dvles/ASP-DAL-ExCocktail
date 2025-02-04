2efb704fc5c7f299b21ad40aa39dfee342e14339
leslie <devotaleslie@gmail.com>
2025-02-04 13:37:46 +0100

    Implemented session-based page visit counter in DemoController
    - Added session variable "CountVisitedPage" to track the number of page visits.
    - Implemented "PlusOne" action to increment the visit count.
    - Updated "Index" action to initialize the visit count in the session on page load.
    - Modified view to display the current visit count from the session.

---
+++ ASP-MVC/Controllers/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_MVC.Controllers
+{
+	public class DemoController : Controller
+	{
+		public IActionResult Index()
+		{
+			HttpContext.Session.SetInt32("CountVisitedPage", 0);
+			return View();
+		}
+
+		public IActionResult PlusOne()
+		{
+			int count = HttpContext.Session.GetInt32("CountVisitedPage")?? 0;
+			count++;
+			HttpContext.Session.SetInt32("CountVisitedPage", count);
+			return View("Index");
+		}
+	}
+}
