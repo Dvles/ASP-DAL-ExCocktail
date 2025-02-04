fd8b29b5846cbfd61b90ee0bb21e01c44d2de7fc
leslie <devotaleslie@gmail.com>
2025-02-04 14:31:30 +0100

    Added ConnectedUser session handling in AuthController Login method

--- ASP-MVC/Controllers/AuthController.cs
+++ ASP-MVC/Controllers/AuthController.cs
@@ -1,7 +1,10 @@
-﻿using ASP_MVC.Models.Auth;
+﻿using ASP_MVC.Models;
+using ASP_MVC.Models.Auth;
+using ASP_MVC.Services;
 using BLL.Entities;
 using Common.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 
 namespace ASP_MVC.Controllers
 {
@@ -31,6 +34,13 @@ namespace ASP_MVC.Controllers
                 if (!ModelState.IsValid) throw new ArgumentException(nameof(form));
                 Guid id = _userService.CheckPassword(form.Email, form.Password);
                 //C'est ici que nous définirons la variable de session
+                ConnectedUser user = new ConnectedUser()
+                {
+                    User_Id = id,
+                    Email = form.Email,
+                    ConnectedAt = DateTime.Now
+                };
+                _sessionManager.Login(user);
                 return RedirectToAction("Details", "User", new { id = id });
             }
             catch (Exception)
